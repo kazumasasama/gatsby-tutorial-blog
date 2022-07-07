592f8c700184e6f6fb96390dcb08c34d39fc3033
Kaz <takaik@yahoo.com>
2022-07-07 14:45:36 -0400

    change siteUrl meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `gatsby-tutorial-blog`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://gatsbytutorialblogmain49295.gatsbyjs.io/`,
   },
   plugins: [
     "gatsby-plugin-image",
