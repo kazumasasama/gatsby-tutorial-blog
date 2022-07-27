13c1ffc82a5598ecd60f0bc4af4474b7735cfcec
Kaz <takaik@yahoo.com>
2022-07-27 16:03:13 -0400

    Finished Gatsby Tutorial Part 7

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,10 @@
 import * as React from 'react';
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
 
 function AboutPage() {
   return (
     <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-      <StaticImage
-        alt=""
-        src="../images/kyoto-tea-ceremony-2-M.jpg"
-      />
     </Layout>
   );
 }
